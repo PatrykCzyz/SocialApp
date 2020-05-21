6d0c0f7b74f561952e7cfcac4f8756f6dc9d51a1
Patryk Czyż <patryk.czyz.ip@icloud.com>
2020-05-21 07:27:01 +0200

    implement custom user in profile

--- TwitterMvc/Dtos/ProfileDto.cs
+++ TwitterMvc/Dtos/ProfileDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.AspNetCore.Identity;
+using TwitterMvc.Enums;
 using TwitterMvc.Models;
 
 namespace TwitterMvc.Dtos
@@ -10,9 +11,19 @@ namespace TwitterMvc.Dtos
         {
             Username = user.UserName;
             Email = user.Email;
+            Name = user.Name;
+            Lastname = user.Lastname;
+            Age = user.Age;
+            Gender = user.Gender;
+            Country = user.Country;
         }
 
         public string Username { get; set; }
         public string Email { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public GenderEnum Gender { get; set; }
+        public string Country { get; set; }
     }
 }

d173b8dd8b85ec078ab3c3657f4b490f0a6cebc7
Patryk Czyż <kontakt@patrykczyz.pl>
2020-05-24 10:27:39 +0200

    PostDto ctor

--- TwitterMvc/Dtos/PostDto.cs
+++ TwitterMvc/Dtos/PostDto.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using TwitterMvc.Models;
+
 namespace TwitterMvc.Dtos
 {
     public class PostDto
     {
+        public PostDto()
+        {
+
+        }
+
+        public PostDto(Post post)
+        {
+            Title = post.Title;
+            Content = post.Content;
+            DateTime = post.DateTime;
+        }
+
         public string Title { get; set; }
         public string Content { get; set; }
         public DateTime DateTime { get; set; }

4c9b664e32d39b32268a0bce860a59cf8cbfa7cf
Patryk Czyż <kontakt@patrykczyz.pl>
2020-05-24 11:02:01 +0200

    datetime in post migration

--- TwitterMvc/Data/Migrations/20200524084237_post datetime.Designer.cs
+++ TwitterMvc/Data/Migrations/20200524084237_post datetime.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TwitterMvc.Data.Context;
 
 namespace TwitterMvc.Data.Migrations
 {
     [DbContext(typeof(IdentityDatabaseContext))]
-    partial class IdentityDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20200524084237_post datetime")]
+    partial class postdatetime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

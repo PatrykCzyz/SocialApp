afa1328dd0f4360f9fde3185cc17ce1e2f37b92e
Patryk Czyż <kontakt@patrykczyz.pl>
2020-07-23 20:57:04 +0200

    error message

--- TwitterMvc/Helpers/ErrorService.cs
+++ TwitterMvc/Helpers/ErrorService.cs
@@ -15,9 +15,9 @@ namespace TwitterMvc.Helpers
             errorList.Add("RemovePostFailed", "You can't remove this post.");
             errorList.Add("EditPostFailed", "You can't edit this post.");
             errorList.Add("UserIsAlreadyFollowed", "You already followed this user.");
-            errorList.Add("UserIsNotFollowed", "You dont follow this user.");
-            errorList.Add("DontHaveFollowers", "You dont have any followers.");
-            errorList.Add("DontHaveFollowing", "You dont follow anyone.");
+            errorList.Add("UserIsNotFollowed", "You don't follow this user.");
+            errorList.Add("DontHaveFollowers", "You don't have any followers.");
+            errorList.Add("DontHaveFollowing", "You don't follow anyone.");
         }
         public string GetError(string errorKey)
         {
